e396f675b39e26ccda7f08e8322b84986e01e17e
cyq7on <cyq7on@qq.com>
2018-03-14 23:28:21 +0800

    finish imageview

--- AVBasePractice/app/src/main/java/com/cyq7on/avbasepractice/MainActivity.java
+++ AVBasePractice/app/src/main/java/com/cyq7on/avbasepractice/MainActivity.java
@@ -1,31 +1,32 @@
 package com.cyq7on.avbasepractice;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+
+import com.cyq7on.avbasepractice.view.MySurfaceView;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView imageView;
+    private MySurfaceView surfaceView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        imageView = findViewById(R.id.imageView);
+        surfaceView = findViewById(R.id.surfaceView);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        showOnImageView();
+    }
+
+    void showOnImageView() {
+        imageView.setImageResource(R.drawable.android_logo);
     }
 
     @Override
---
+++ AVBasePractice/app/src/main/java/com/cyq7on/avbasepractice/view/MySurfaceView.java
@@ -0,0 +1,70 @@
+package com.cyq7on.avbasepractice.view;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import com.cyq7on.avbasepractice.R;
+
+/**
+ * Created by cyq7on on 18-3-13.
+ */
+
+public class MySurfaceView extends SurfaceView implements SurfaceHolder.Callback {
+    private SurfaceHolder surfaceHolder;
+    private Canvas canvas;
+
+    public MySurfaceView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    private void init() {
+        surfaceHolder = getHolder();
+        surfaceHolder.addCallback(this);
+        //更改背景为透明
+//        setZOrderOnTop(true);
+        setFocusable(true);
+        setFocusableInTouchMode(true);
+        setClickable(true);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+
+        Paint paint = new Paint();
+        paint.setColor(Color.RED);
+        paint.setStyle(Paint.Style.FILL);
+        paint.setStrokeWidth(10);
+
+        canvas = holder.lockCanvas();
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.android_logo);
+
+//        BitmapFactory.Options opt = new BitmapFactory.Options();
+//        opt.inPreferredConfig = Bitmap.Config.RGB_565;
+//        opt.inPurgeable = true;
+//        opt.inInputShareable = true;
+//        InputStream is = getResources().openRawResource(+R.drawable.android_logo);
+//        Bitmap bitmap =  BitmapFactory.decodeStream(is, null, opt);
+
+        canvas.drawBitmap(bitmap, 0, 0, paint);
+        surfaceHolder.unlockCanvasAndPost(canvas);
+
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        holder.removeCallback(this);
+    }
+}
